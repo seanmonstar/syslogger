38e0d9e4dc3ab1596715f3760290561bde89b4fd
Sam Shull <sshull@zendesk.com>
2014-06-16 14:08:37 -0700

    Allow the ability to specify syslog message format

--- test/syslogger.js
+++ test/syslogger.js
@@ -22,8 +22,7 @@ module.exports = {
         server.once('message', function(buf) {
           var str = buf.toString();
 
-          assert(/^<\d+>/.test(str), str);
-          assert(/<\d+> \w{3} \d{2} \d{2}:\d{2}:\d{2}/.test(str), str);
+          assert(/^<\d+>\w{3} \d{2} \d{2}:\d{2}:\d{2}/.test(str), str);
           assert(/SysLogger\$\d+/.test(str), str);
           assert(new RegExp("[" + process.pid + "]:").test(str), str);
           assert(/foo$/.test(str));
